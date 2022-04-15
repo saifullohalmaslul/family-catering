49d2bc82c8ae168624f9b609c7c1efdf07093207
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-15 13:02:36 +0000

    Generate model for Menu

---
+++ db/migrate/20220415125953_create_menus.rb
@@ -0,0 +1,11 @@
+class CreateMenus < ActiveRecord::Migration[7.0]
+  def change
+    create_table :menus do |t|
+      t.string :name
+      t.float :price
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/menus.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :menu do
+    name { "MyString" }
+    price { 1.5 }
+    description { "MyText" }
+  end
+end
