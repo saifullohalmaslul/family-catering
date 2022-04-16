e8f106af995496651a54ab9b9b7fe758952c55ba
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-16 17:34:59 +0000

    Add association for Category, it should have many Menus

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+
+  has_many :menu_categories
+  has_many :menus, through: :menu_categories
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Category, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
   end
+
+  describe 'associations' do
+    it { should have_many(:menus) }
+  end
 end
