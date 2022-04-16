2312eec8ee19dcdb76f0e60bdcf8df1ab6fbc415
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-16 15:33:51 +0000

    Add validation for Menu, description cannot have more than 150 character

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -6,5 +6,6 @@ RSpec.describe Menu, type: :model do
     it { should validate_uniqueness_of(:name) }
     it { should validate_presence_of(:price) }
     it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0.01) }
+    it { should validate_length_of(:description).is_at_most(150) }
   end
 end
