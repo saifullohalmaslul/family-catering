56def789f98bb44423abae1229ff589ab3ceafa4
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-16 15:24:22 +0000

    Add validation for Menu, price cannot be less than 0.01

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,4 @@
 class Menu < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe Menu, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
     it { should validate_presence_of(:price) }
+    it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0.01) }
   end
 end
