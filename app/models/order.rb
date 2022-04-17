23f19896a799e77a23b7d3b2b6e3d92caac009d2
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-17 05:35:36 +0000

    Add association for Order, it should have many order_details

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ApplicationRecord
   validates :status, inclusion: { in: %w[NEW PAID CANCELED] }
+  has_many :order_details
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Order, type: :model do
   describe 'validations' do
     it { should validate_inclusion_of(:status).in_array(['NEW', 'PAID', 'CANCELED']) }
   end
+
+  describe 'associations' do
+    it { should have_many(:order_details) }
+  end
 end
