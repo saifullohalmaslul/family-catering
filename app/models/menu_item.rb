fa9ac2aaa956accc612c0bd8fc31bae1e93cbacf
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-18 12:55:32 +0000

    Add validation for MenuItem, category must exist

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -2,6 +2,7 @@ class MenuItem < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
   validates :description, length: { maximum: 150 }
+  validates :categories, presence: true
 
   has_many :menu_categories
   has_many :categories, through: :menu_categories
--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe MenuItem, type: :model do
     it { should validate_presence_of(:price) }
     it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0.01) }
     it { should validate_length_of(:description).is_at_most(150) }
+    it { should validate_presence_of(:categories) }
   end
 
   describe 'associations' do
