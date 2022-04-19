f111e99d7fea63dba87d4092536ccdfbc2e0adf9
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-19 16:03:33 +0000

    Add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+categories = Category.create([{ name: "Indonesian" }, { name: "Breakfast" }])
+nasi_uduk = MenuItem.create(name: "Nasi Uduk", price: 10_000.0, description: "Some description here", categories: categories)
+
+orange_juice = MenuItem.create(name: "Orange Juice", price: 12_500.0, description: "Some other description", categories: [Category.create(name: "Beverage")])
+
+Order.create(email: 'me@example.com', menu_items: [nasi_uduk, orange_juice])
