c8f7f830554e0a8b547508e31204589a13b40fae
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-20 15:54:51 +0000

    Refactor OrdersController #create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,13 +16,32 @@ class OrdersController < ApplicationController
       params[:details].each do |detail|
         menu_item = MenuItem.find(detail[:menu_item])
         quantity = detail[:quantity].to_f
-        @order.menu_items << menu_item
-        OrderDetail.find_by(order: @order, menu_item: menu_item)
-          .update(price: menu_item.price, quantity: quantity)
-        total_price += menu_item.price * quantity
+        
+        fill_order_detail(@order, menu_item, quantity)
       end
 
-      @order.total_price = total_price
+      fill_total_price(@order)
     end
+  end
+
+  private
+
+  def fill_order_detail(order, menu_item, quantity)
+    OrderDetail.create(
+      order: order, 
+      menu_item: menu_item, 
+      price: menu_item.price, 
+      quantity: quantity
+    )
+  end
+
+  def fill_total_price(order)
+    total_price = 0
+    
+    order.order_details.each do |one_item| 
+      total_price += one_item.price * one_item.quantity
+    end
+
+    order.total_price = total_price
   end
 end
