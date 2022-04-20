2039c6a5e8bd4779013e93472ea271143eb3e0bf
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-20 07:14:44 +0000

    Add pending tests to MenuItems controller spec

--- spec/requests/menu_items_spec.rb
+++ spec/requests/menu_items_spec.rb
@@ -1,7 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe "MenuItems", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe MenuItemsController, type: :controller do
+  describe 'GET #index' do
+    it "populates an array of all menu_items"
+    it "renders the :index template"
+  end
+
+  describe 'GET #show' do
+    it "assigns the requested menu_item to @menu_item"
+    it "renders the :show template"
+  end
+
+  describe 'GET #new' do
+    it "assigns a new menu_item to @menu_item"
+    it "renders the :new template"
+  end
+
+  describe 'GET #edit' do
+    it "assigns the requested menu_item to @menu_item"
+    it "renders the :edit template"
+  end
+
+  describe 'POST #create' do
+    context "with valid attributes" do
+      it "saves the new menu_item in the database"
+      it "redirects to menu_items#show"
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new menu_item in the database"
+      it "re-renders the :new template"
+    end
+  end
+
+  describe 'PATCH #update' do
+    context "with valid attributes" do
+      it "locates the requested @menu_item"
+      it "changes @menu_item's attributes"
+      it "redirects to the menu_item"
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the updated menu_item in the database'
+      it 're-renders the edit template'
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it "deletes the menu_item from the database"
+    it "redirects to menu_items#index"
   end
 end
