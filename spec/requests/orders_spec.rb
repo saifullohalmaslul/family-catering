b190c4b2a1c080394e76f18265354dacb83be55d
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-21 03:29:28 +0000

    Clean OrdersController spec, assign params into order_requirements

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe OrdersController, type: :controller do
+  before do
+    @order_requirements = {
+      order: attributes_for(:order), 
+      details: [
+        { menu_item: create(:menu_item_with_categories, price: 12_000), quantity: 1 },
+        { menu_item: create(:menu_item_with_categories, price: 10_000), quantity: 3 }
+      ]
+    }
+  end
+  
   describe 'GET #new' do
     it "assigns a new order to @order" do
       get :new
@@ -17,53 +27,38 @@ RSpec.describe OrdersController, type: :controller do
     it "assigns the requested order to @order" do
       order = create(:order_with_menu_items)
       get :edit, params: { id: order }
+      
       expect(assigns(:order)).to eq order
     end
     
     it "renders the :edit template" do
       order = create(:order_with_menu_items)
       get :edit, params: { id: order }
+      
       expect(response).to render_template :edit
     end
   end
 
   describe 'POST #create' do
     it "re-renders the :new template" do
-      post :create, params: { 
-        order: attributes_for(:order), 
-        details: [
-          { menu_item: create(:menu_item_with_categories, price: 12_000), quantity: 1 },
-          { menu_item: create(:menu_item_with_categories, price: 10_000), quantity: 3 }
-        ]
-      }
-
+      post :create, params: @order_requirements
       expect(response).to render_template :new
     end
     
     context "with valid attributes" do
       it "saves the new order in the database" do
         expect{
-          post :create, params: { 
-            order: attributes_for(:order), 
-            details: [
-              { menu_item: create(:menu_item_with_categories, price: 12_000), quantity: 1 },
-              { menu_item: create(:menu_item_with_categories, price: 10_000), quantity: 3 }
-            ]
-          }
+          post :create, params: @order_requirements
         }.to change(Order, :count).by(1)
       end
     end
 
     context "with invalid attributes" do
       it "does not saves the new order in the database" do
+        @order_requirements[:order] = attributes_for(:invalid_order)
+        
         expect{
-          post :create, params: { 
-            order: attributes_for(:invalid_order), 
-            details: [
-              { menu_item: create(:menu_item_with_categories, price: 12_000), quantity: 1 },
-              { menu_item: create(:menu_item_with_categories, price: 10_000), quantity: 3 }
-            ]
-          }
+          post :create, params: @order_requirements
         }.not_to change(Order, :count)
       end
     end
@@ -74,15 +69,9 @@ RSpec.describe OrdersController, type: :controller do
     
     context "with valid attributes" do
       it "locates the requested @order" do
-        patch :update, params: { 
-          id: order,
-          order: attributes_for(:order), 
-          details: [
-            { menu_item: create(:menu_item_with_categories, price: 12_000), quantity: 1 },
-            { menu_item: create(:menu_item_with_categories, price: 10_000), quantity: 3 }
-          ]
-        }
-
+        @order_requirements[:id] = order
+        patch :update, params: @order_requirements
+        
         expect(assigns(:order)).to eq order
       end
     end
