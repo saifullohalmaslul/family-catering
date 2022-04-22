a65c8228a1e4e2dc3507de16374f96ff19d813b2
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-22 19:20:33 +0000

    Generate Reports controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'reports/index', to: 'reports#index', as: 'reports'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
