649fde5579c8c43c5580069e0115e71aa1c6cdee
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-22 12:24:17 +0000

    Remove total_price from order factory

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :order do
     email { "halo@example.com" }
-    total_price { 120_000.59 }
 
     factory :order_with_menu_items do
       menu_items { [association(:menu_item)] }
