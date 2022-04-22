2db8f79873b6accc0ef2a4e1243cee7505aeb1ab
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-22 11:47:12 +0000

    OrderDetail should save the price of the menu_item

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,12 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :menu_item
+
+  before_save :save_price
+
+  private
+
+  def save_price
+    self.price = self.menu_item.price
+  end
 end
--- spec/models/order_detail_spec.rb
+++ spec/models/order_detail_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe OrderDetail, type: :model do
     it { should belong_to(:order) }
     it { should belong_to(:menu_item) }
   end
+
+  it 'should save the price of menu item' do
+    order_detail = create(:order_detail)
+
+    expect(order_detail.price).to eq order_detail.menu_item.price
+  end
 end
