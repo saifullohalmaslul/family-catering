baf49137021980b6c2c6aa8f5b5363bdc6e267b3
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-23 01:02:13 +0000

    Fix reports#index route, omit index from path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'reports/index', to: 'reports#index', as: 'reports'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  get '/reports', to: 'reports#index', as: 'reports'
   resources :menu_items
   resources :orders, only: [:index, :new, :edit, :create, :update, :destroy]
 end
