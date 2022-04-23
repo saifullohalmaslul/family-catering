7753a06c5166cf1a120bc6b8443ae24574b568a5
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-23 01:44:39 +0000

    Add form for inputting date on reports index view

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,6 +2,7 @@ class ReportsController < ApplicationController
   def index
     if params[:date]
       @orders = Order.where(status: 'PAID', created_at: params[:date].to_date.all_day)
+      @date = params[:date]
     else
       @orders = Order.where(status: 'PAID', created_at: Date.today.all_day)
     end
