8950a3f991166a8e067c17926d95cac73908ad84
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-23 00:18:58 +0000

    GET reports#index with no params should populate paid orders of current day

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,5 @@
 class ReportsController < ApplicationController
   def index
+    @orders = Order.where(status: 'PAID', created_at: Date.today.all_day)
   end
 end
--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -1,10 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe ReportsController, type: :controller do
+  include ActiveSupport::Testing::TimeHelpers
+  
   describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    context 'with no params' do
+      it 'populates array of paid orders of current day' do
+        orders_yesterday = [
+          create(:order),
+          create(:order),
+          create(:order)
+        ]
+
+        travel 1.day
+
+        orders_today = [
+          create(:order, status: 'PAID'),
+          create(:order),
+          create(:order, status: 'PAID')
+        ]
+
+        get :index
+        expect(assigns(:orders)).to match_array [orders_today[0], orders_today[2]]
+      end
     end
   end
 end
