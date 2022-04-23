200e64ec94953efeeac2aab6632646f97dd52fb6
Saifulloh Almaslul <saifullohalmaslul444@gmail.com>
2022-04-23 01:53:15 +0000

    Define orders#index as root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "orders#index"
   
   get '/reports', to: 'reports#index', as: 'reports'
   resources :menu_items
